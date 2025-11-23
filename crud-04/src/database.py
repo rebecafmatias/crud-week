02ba55fd3662496651d7ea6713009c2605a69672
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-23 12:49:14 -0300

    completing database.py file

---
+++ crud-04/src/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///store.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
+Base = declarative_base
+
