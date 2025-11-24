5aac85665bc0e6dcd49bb9e02209c4dc44fabfe3
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-24 20:17:03 -0300

    completing models.py file

--- crud-04/src/database.py
+++ crud-04/src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///store.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///chain.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,
---
+++ crud-04/src/models.py
@@ -0,0 +1,32 @@
+from database import Base
+from sqlalchemy import Column, String, Integer, Date, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+class StoreModel(Base):
+    __tablename__ = "store"
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String,nullable=False)
+    address = Column(String,nullable=False)
+    zipcode = Column(Integer, nullable=False)
+    city = Column(String,nullable=False)
+    state = Column(String,nullable=False)
+    createdAt = Column(DateTime(timezone=True),server_default=func.now())
+    updatedAt = Column(DateTime(timezone=True),onupdate=func.now())
+
+    employees = relationship("Employee", back_populates="employee") 
+
+class EmployeeModel(Base):
+    __tablename__ = "employee"
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String,nullable=False)
+    birthdate = Column(Date,nullable=False)
+    position = Column(String,nullable=False)
+    startDate = Column(Date,nullable=False)
+    storeId = Column(Integer,ForeignKey("store.id"))
+    createdAt = Column(DateTime(timezone=True),server_default=func.now())
+    updatedAt = Column(DateTime(timezone=True),onupdate=func.now())
+
+    stores = relationship("Store", back_populates="store")
