98cfe6d60ac5f2317f99279dd6918ee71b7714e3
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-26 20:49:34 -0300

    simplifying create func

--- crud-04/src/crud.py
+++ crud-04/src/crud.py
@@ -1,16 +1,12 @@
 from sqlalchemy.orm import Session
-import models
+from typing import Any
 
-def create_employee(db: Session, employee_db: dict):
-    db_to_add = models.EmployeeModel(**employee_db)
+def create_record(db: Session, model_class: Any, db_value: dict):
+    db_to_add = model_class(**db_value)
 
     db.add(db_to_add)
     db.commit()
     db.refresh(db_to_add)
 
-def create_store(db:Session, store_db: dict):
-    db_to_add = models.StoreModel(**store_db)
-    
-    db.add(db_to_add)
-    db.commit()
-    db.refresh(db_to_add)
+    return db_to_add
+
