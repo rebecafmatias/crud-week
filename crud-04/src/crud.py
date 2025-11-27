f6652a33e5fe4591f53b6506446158edf4d47e4d
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-26 21:05:23 -0300

    add deleting func

--- crud-04/src/crud.py
+++ crud-04/src/crud.py
@@ -16,12 +16,25 @@ def update_record(db: Session, model_class: Any, id: int, db_value: dict):
     if db_to_update:
         for key,value in db_value.items():
             setattr(db_to_update,key,value)
-            
+
             db.commit()
             db.refresh(db_to_update)
 
-        else:
-            print(f"This id {id} was not found.")
-            return None
+    else:
+        print(f"This id {id} was not found.")
+        return None
     
     return db_to_update
+
+def delete_record(db: Session, model_class: Any, id: int):
+    db_to_delete = db.query(model_class).filter(model_class.id==id).first()
+
+    if db_to_delete:
+        db.delete(db_to_delete)
+        db.commit()
+    
+    else:
+        print(f"This id {id} was not found.")
+        return None
+    
+    return db_to_delete
