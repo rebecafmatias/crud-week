1373784632e316855484983c2dc767f96c6f0998
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-26 21:36:19 -0300

    add controller to main.py

--- crud-04/src/main.py
+++ crud-04/src/main.py
@@ -1,8 +1,25 @@
-from database import engine, Base
+from database import engine, Base, SessionLocal
 import models
+import controller
+from sqlalchemy.orm import Session
+from typing import Any
 
-def main():
+def main(db: Session, model_class: Any, crud_type: str, id: int, db_value: dict):
     Base.metadata.create_all(bind=engine)
+    
+    controller.crud_controller(db,model_class,crud_type,id,db_value)
 
 if __name__ == "__main__":
-    main()
+    db = SessionLocal
+    model_class = models.StoreModel
+    crud_type = "create"
+    id = None
+    db_value = {
+        "name": "Big Book Store",
+        "address": "Av. Beira Mar Norte, 1500",
+        "zipcode": 88015200, 
+        "city": "Florianopolis",
+        "state": "SC"
+    }
+
+    main(db, model_class, crud_type, id, db_value)
