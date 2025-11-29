8c4f9be75f4d583cb67eefc414bcdbcffd8c27b8
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-29 19:18:45 -0300

    add create batch func

--- crud-05/src/crud.py
+++ crud-05/src/crud.py
@@ -25,6 +25,16 @@ def create_record(db: Session, model_class: Any, data_value: dict):
 
     return result
 
+def create_batch_record(db: Session, model_class: Any, data_value: list[dict]):
+    result = insert(model_class).values(data_value)
+
+    db.execute(result)
+    db.commit()
+    
+    print(f"Batch insert finalizado com sucesso!")
+    
+    return len(data_value) 
+
 def update_record(db: Session, model_class: Any, id: int, data_value: dict):
     result = db.query(model_class).filter(model_class.id==id).first()
 
