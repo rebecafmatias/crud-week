53b3542933b0aeb36e0aadbba3a0db630030339c
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-29 18:40:17 -0300

    add create func

--- crud-05/src/crud.py
+++ crud-05/src/crud.py
@@ -1,12 +1,21 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import insert
 from typing import Any
 
-def get_all_data(db: Session, model_class: Any):
+def get_all(db: Session, model_class: Any):
     result = db.query(model_class).all()
 
     return result
 
-def get_data_by_id(db: Session, model_class: Any, id: int):
+def get_by_id(db: Session, model_class: Any, id: int):
     result = db.query(model_class).filter(model_class.id==id).first()
 
     return result
+
+def create_record(db: Session, model_class: Any, data_value: dict):
+    result = model_class(**data_value)
+
+    db.add(result)
+    db.commit()
+    db.refresh(result)
+    
