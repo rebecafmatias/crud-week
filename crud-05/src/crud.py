ec1cec9b62ec511bd029af24d1dbf9a1ca1ea82d
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-29 18:29:11 -0300

    add crud with get funcs

--- crud-05/src/crud.py
+++ crud-05/src/crud.py
@@ -1,3 +1,12 @@
 from sqlalchemy.orm import Session
-from database import Base
+from typing import Any
 
+def get_all_data(db: Session, model_class: Any):
+    result = db.query(model_class).all()
+
+    return result
+
+def get_data_by_id(db: Session, model_class: Any, id: int):
+    result = db.query(model_class).filter(model_class.id==id).first()
+
+    return result
