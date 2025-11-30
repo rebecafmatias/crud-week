0e11a5868eb67ad9ad444fd284e05237bc6765a1
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-11-30 16:52:28 -0300

    finished controller.py file

--- crud-05/src/controller.py
+++ crud-05/src/controller.py
@@ -14,3 +14,17 @@ def crud_controller(db: Session, model_class: Any, crud_action: str, id: int, da
     
     if crud_action == "create":
         result = crud.create_record(db,model_class,data_value)
+
+    elif crud_action == "update":
+        result = crud.update_record(db,model_class,id,data_value)
+
+    elif crud_action == "delete":
+        result = crud.delete_record(db,model_class,id)
+
+    elif crud_action == "get":
+        if id == None or id == 0:
+            result = crud.get_all(db,model_class)
+        else:
+            result = crud.get_by_id(db,model_class,id)
+    
+    return result
